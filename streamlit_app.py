e5c3d594f10638197a83e481a8e9075c8bd4e9ca
Raluca Dragomir <102033725+ralucadrago@users.noreply.github.com>
2022-03-21 14:54:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 st.title("Welcome to my first streamlit app!")
 st.header("This is fun")
-st.text_input("number1")
-st.text_input("number2")
+nr1=st.text_input("number1")
+nr2=st.text_input("number2")
+
+st.title(int(nr1)+int(nr2))
