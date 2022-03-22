d90f81592dfbfc0db8c77bd3d22d283f8713ce41
a-pearlman <96663441+a-pearlman@users.noreply.github.com>
2022-03-22 11:05:05 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
+import statsmodels.tools
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 st.title("Oh hey its an app")
 
