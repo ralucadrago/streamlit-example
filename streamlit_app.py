b3ead1d904a8ef4301e1b90c43c96bbedbcfec4b
Raluca Dragomir <102033725+ralucadrago@users.noreply.github.com>
2022-03-30 14:38:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,31 +21,7 @@ cloud.set_index('date', inplace=True)
 
 snow.set_index('date', inplace=True)
 
-def nice_day(year,month,day,acceptable_temp=10):
-    year=int(year)
-    month=int(month)
-    day=int(day)
-    new_temp=temp.iloc[temp.index.get_loc(datetime.datetime(year,month,day),method='nearest')].values
-    new_cloud=cloud.iloc[cloud.index.get_loc(datetime.datetime(year,month,day),method='nearest')].values
-    
-    temp_est=new_temp[0]
-    cloud_est=new_cloud[0]
-    
-    if temp_est < acceptable_temp:
-        if cloud_est > 0.6:
-            return 'Not a nice day!'
-        elif cloud_est <= 0.6:
-            return 'A nice but cold day!'
-        else:
-            return 'uhm you broke me'
-        
-    if temp_est >= acceptable_temp:
-        if cloud_est > 0.6:
-            return 'Warm but not nice'
-        elif cloud_est <= 0.6:
-            return 'Congrats! You unlocked a nice day!'
-        else:
-            return 'uhm you broke me'
+
         
 def snow_day(year,month,day):
     year=int(year)
@@ -83,6 +59,33 @@ acceptable_temperature=10
 
 if button:
     acceptable_temperature=st.text_input("fine... what temperature do you deem acceptable?")
+    
+    
+def nice_day(year,month,day,acceptable_temp=10):
+    year=int(year)
+    month=int(month)
+    day=int(day)
+    new_temp=temp.iloc[temp.index.get_loc(datetime.datetime(year,month,day),method='nearest')].values
+    new_cloud=cloud.iloc[cloud.index.get_loc(datetime.datetime(year,month,day),method='nearest')].values
+    
+    temp_est=new_temp[0]
+    cloud_est=new_cloud[0]
+    
+    if temp_est < acceptable_temp:
+        if cloud_est > 0.6:
+            return 'Not a nice day!'
+        elif cloud_est <= 0.6:
+            return 'A nice but cold day!'
+        else:
+            return 'uhm you broke me'
+        
+    if temp_est >= acceptable_temp:
+        if cloud_est > 0.6:
+            return 'Warm but not nice'
+        elif cloud_est <= 0.6:
+            return 'Congrats! You unlocked a nice day!'
+        else:
+            return 'uhm you broke me'
     
 final=nice_day(year,month,day,acceptable_temperature)
 
